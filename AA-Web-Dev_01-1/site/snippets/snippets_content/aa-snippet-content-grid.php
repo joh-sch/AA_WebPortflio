610bea7da0befcf479c2abddfab73619c6515b8e
JoSch <j.schmoll@udk-berlin.de>
2018-08-04 09:09:07 +0200

    button clickable

--- AA-Web-Dev_01-1/site/snippets/snippets_content/aa-snippet-content-grid.php
+++ AA-Web-Dev_01-1/site/snippets/snippets_content/aa-snippet-content-grid.php
@@ -12,15 +12,19 @@ $projects = page('aa-projekte')->children()->visible();
 		<?php if($image = $project->images()->sortBy('sort', 'asc')->first()): $thumb = $image ?>
 
 			<div class="grid-item <?= $project->style() ?>">
-				<a href="<?= $project->url() ?>">
 					<div class="grid-item--overlay">
+
+						<!-- –– Project Link –– -->
+							<a href="<?= $project->url() ?>"></a>
+						<!-- –––––––––––––––––– -->
+
 						<!-- »Show similar« Btn -->
-							<div class="grid-item--btn text--project">
+							<div class="grid-item--btn text--project z--high" onClick="alert('test')">
 								<span>show similar</span>
 							</div>
 						<!-- –––––––––––––––––– -->
+
 					</div>
-				</a>
 				<img src="<?= $thumb->url() ?>" alt="Thumbnail for <?= $project->title()->html() ?>" class="showcase-image" />
 		  </div>
 
