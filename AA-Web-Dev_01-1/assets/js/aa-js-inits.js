c04a359f2c806c6a9c5b0b3b1d56680f6b57d34a
JoSch <j.schmoll@udk-berlin.de>
2018-08-07 19:36:38 +0200

    Setup of Animation #2 ("Zunge raus")

--- AA-Web-Dev_01-1/assets/js/aa-js-inits.js
+++ AA-Web-Dev_01-1/assets/js/aa-js-inits.js
@@ -14,17 +14,20 @@
 			var iconContainer = $('#MainMenuButton')
 			var iconImage			=	$('#brandIcon')
 
-			// Load images of set
+			// Load location prefix (path)
 			var iconsPathPre	= '/assets/images/brandicon/';
 
-				// Set #1
+			//////////////////////////////////////////////////
+
+			// Animation #1
+			function icon_animation01() {
+
+				// Animation #1 – Frames
 				var iconSet1_frame1	= iconsPathPre + 'icon-01/Portrait-Icon-01-Augenrollen-01-1_1.svg';
 				var iconSet1_frame2	= iconsPathPre + 'icon-01/Portrait-Icon-01-Augenrollen-01-1_2.svg';
 				var iconSet1_frame3	= iconsPathPre + 'icon-01/Portrait-Icon-01-Augenrollen-01-1_3.svg';
 				var iconSet1_frame4	= iconsPathPre + 'icon-01/Portrait-Icon-01-Augenrollen-01-1_4.svg';
-			
-			// Animation #1
-			function icon_animation01() {
+
 				// Loading initial frame (the default icon)
 				console.log('%cBrand-Icon: Loading 1st frame of first set …', 'background:tomato ; color: white');
 				iconImage.attr("src", iconSet1_frame1);
@@ -56,8 +59,48 @@
 				setInterval(icon_animation01_play, 15000)
 			}
 
+			//////////////////////////////////////////////////
+
+			// Animation #2
+			function icon_animation02() {
+
+				// Animation #2 – Frames
+				var iconAnim2_frame1	= iconsPathPre + 'icon-02/Portrait-Icon-02-ZungeRaus-02-1_1.svg';
+				var iconAnim2_frame2	= iconsPathPre + 'icon-02/Portrait-Icon-02-ZungeRaus-02-1_2.svg';
+				var iconAnim2_frame3	= iconsPathPre + 'icon-02/Portrait-Icon-02-ZungeRaus-02-1_3.svg';
+
+				// Loading initial frame (the default icon)
+				console.log('%cBrand-Icon: Loading 1st frame (default icon) …', 'background:tomato ; color: white');
+				iconImage.attr("src", iconAnim2_frame1);
+				
+				// Set up frames
+				function icon_animation02_play() {
+					setTimeout(function() {
+						console.log('%cBrand-Icon: Loading Animation #2, 2nd frame …', 'background:Lightgreen ; color: black');
+						iconImage.attr("src", iconAnim2_frame2);
+					}, 140);
+					/////////////////////////
+					setTimeout(function() {
+						console.log('%cBrand-Icon: Loading Animation #2, 3nd frame …', 'background:Lightgreen ; color: black');
+						iconImage.attr("src", iconAnim2_frame3);
+					}, 280);
+					/////////////////////////
+					setTimeout(function() {
+						console.log('%cBrand-Icon: Loading Animation #2, last frame (default icon) …', 'background:Lightblue ; color: black');
+						iconImage.attr("src", iconAnim2_frame1);
+					}, 560);
+				}
+
+				// Play frames
+				setInterval(icon_animation02_play, 5000)
+			}
+
+
+			//////////////////////////////////////////////////
+
+
 			// Call Animation #1
-			icon_animation01();
+			icon_animation02();
 		}
 
 //	–––––––––––––––––––––––––––––––
