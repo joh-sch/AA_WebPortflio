795e4b35e660cae86e0b384c469b3026847a857c
JoSch <j.schmoll@udk-berlin.de>
2018-09-08 07:26:14 +0200

    added noindex + nofollow meta-tags

--- AA-Web-Dev_01-1/site/snippets/snippets_header/aa-snippet-header.php
+++ AA-Web-Dev_01-1/site/snippets/snippets_header/aa-snippet-header.php
@@ -2,6 +2,7 @@
 <html lang="<?= site()->language() ? site()->language()->code() : 'en' ?>">
 <head>
 
+	<meta name="robots" content="noindex,nofollow">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
