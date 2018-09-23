924b8fffb93e9bc5641ef4d58b3ec45a1e514b29
JoSch <j.schmoll@udk-berlin.de>
2018-09-23 09:21:38 +0200

    re-build grid-building loop based on CSS-only masonry

--- AA-Web-Dev_01-1/site/snippets/snippets_content/aa-snippet-content-grid.php
+++ AA-Web-Dev_01-1/site/snippets/snippets_content/aa-snippet-content-grid.php
@@ -5,8 +5,19 @@ $projects = page('aa-projekte')->children()->visible();
 
 ?>
 
-<div class="grid" id="contentHome">
+<div class="masonry" id="contentHome">
+
+	<?php foreach($projects as $project): ?>
+
+		<?php if($image = $project->images()->sortBy('sort', 'asc')->first()): $thumb = $image ?>
+			<div class="masonry--item <?= $project->style() ?> <?= $project->similar() ?>">
+						
 
 
+					<img src="<?= $project->images()->filterBy('filename', '*=', 'cover')->first()->url() ?>" alt="Thumbnail for <?= $project->title()->html() ?>" class="showcase-image" />
+				</div>
+		<?php endif ?>
+
+	<?php endforeach ?>
 
 </div>
