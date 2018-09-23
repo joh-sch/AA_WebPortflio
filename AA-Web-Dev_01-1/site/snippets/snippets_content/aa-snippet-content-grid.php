56b2f421a02891f8b29a26bf96a884a6d30fae4e
JoSch <j.schmoll@udk-berlin.de>
2018-09-23 11:46:49 +0200

    layout revision of grid-item overlays (based on 4col-layout)

--- AA-Web-Dev_01-1/site/snippets/snippets_content/aa-snippet-content-grid.php
+++ AA-Web-Dev_01-1/site/snippets/snippets_content/aa-snippet-content-grid.php
@@ -10,12 +10,40 @@ $projects = page('aa-projekte')->children()->visible();
 	<?php foreach($projects as $project): ?>
 
 		<?php if($image = $project->images()->sortBy('sort', 'asc')->first()): $thumb = $image ?>
-			<div class="masonry--item <?= $project->style() ?> <?= $project->similar() ?>">
-						
+			<div class="masonry--item <?= $project->style() ?> <?= $project->similar() ?>">		
+				<div class="grid-item--overlay">
+					<!-- –– Project Link –– -->
+						<a href="<?= $project->url() ?>"></a>
+					<!-- –––––––––––––––––– -->
 
+					<!-- – Project Info –– -->
+					<div class="grid-item--overlay--text-container">
 
-					<img src="<?= $project->images()->filterBy('filename', '*=', 'cover')->first()->url() ?>" alt="Thumbnail for <?= $project->title()->html() ?>" class="showcase-image" />
+						<div class="grid-item--overlay--text text--headline">
+							<span><?= $project->title()->kirbytext() ?></span>
+							<span><?= $project->year()->kirbytext() ?></span>
+						</div>
+
+						<div class="grid-item--overlay--text text--project text--italic ">
+							<span><?= $project->client()->kirbytext() ?></span>
+						</div>
+						<div class="grid-item--overlay--text text--project">
+							<span><?= $project->description()->kirbytext() ?></span>
+						</div>
+
+					</div>
+					<!-- –––––––––––––––––– -->
+
+					<!-- »Show similar« Btn -->
+						<div class="grid-item--overlay--btn text--headline z--high filters--tag" onClick="">
+							<button data-filter=".<?= $project->similar() ?>">
+								<span>show similar</span>
+							</button>
+						</div>
+					<!-- –––––––––––––––––– -->
 				</div>
+				<img src="<?= $project->images()->filterBy('filename', '*=', 'cover')->first()->url() ?>" alt="Thumbnail for <?= $project->title()->html() ?>" class="showcase-image" />
+			</div>
 		<?php endif ?>
 
 	<?php endforeach ?>
