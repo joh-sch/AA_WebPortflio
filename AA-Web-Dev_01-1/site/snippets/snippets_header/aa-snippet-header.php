fe11144d82bb3710ee9a2249fca541a914b9fa5d
JoSch <j.schmoll@udk-berlin.de>
2018-10-03 08:36:47 +0200

    added separate stylesheet for variables

--- AA-Web-Dev_01-1/site/snippets/snippets_header/aa-snippet-header.php
+++ AA-Web-Dev_01-1/site/snippets/snippets_header/aa-snippet-header.php
@@ -21,6 +21,9 @@
 	
 	<!-- Reset -->
 		<?= css('assets/css/aa-css-reset.css') ?>
+
+	<!-- Variables -->
+		<?= css('assets/css/aa-css-variables.css') ?>
 	
 	<!-- Media Queries -->
 		<?= css('assets/css/aa-css-mediaQueries.css') ?>
