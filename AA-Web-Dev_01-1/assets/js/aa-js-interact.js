9b8331803cd5890941ecaab837307f9e4242a9af
JoSch <j.schmoll@udk-berlin.de>
2018-10-19 15:29:52 +0200

    modified filtering-function to sort all items back into grid on second filter-button click

--- AA-Web-Dev_01-1/assets/js/aa-js-interact.js
+++ AA-Web-Dev_01-1/assets/js/aa-js-interact.js
@@ -106,43 +106,83 @@ function mainmenu_close() {
 //	––––––––––––––––––––––––––––––––
 
 // Filter-Button(s)
+var unmatched = $();
+var matched = $();
+
+var detached_matched = [];
+var detached_unmatched = [];
+
 $("document").ready(function() {
   var button = $("#FilterMenu").children(
     ".menu--navbar--bottom--button.default"
   );
   //
+  var gridItems = $(".grid--item");
   var column1 = $("#column1");
   var column2 = $("#column2");
   var column3 = $("#column3");
   //
   button.click(function() {
-    var gridItems = $(".grid--item");
     var filter = "." + $(this).data("filter");
-    //
+    // Filter items
     var unmatched = gridItems.not(filter);
     var matched = gridItems.filter(filter);
     //
     if (filter == ".all") {
-      alert(filter);
     } else {
-      var columnCounter = 1;
-      //
-      $.each(matched, function(i, v) {
-        if (columnCounter == 1) {
-          column1.append(v);
-          columnCounter++;
-        } else {
-          if (columnCounter == 2) {
-            column2.append(v);
+      if ($(this).hasClass("active")) {
+        // Remove all items from grid
+        unmatched.detach();
+        matched.detach();
+        // Set button inactive
+        $(this).removeClass("active");
+        // Merge matched & unmatched items
+        var allItems = $.merge(unmatched, matched);
+        // Sort all items back into columns
+        var columnCounter = 1;
+        //
+        $.each(allItems, function(i, v) {
+          if (columnCounter == 1) {
+            column1.append(v);
             columnCounter++;
           } else {
-            if (columnCounter == 3) {
-              column3.append(v);
-              columnCounter = 1;
+            if (columnCounter == 2) {
+              column2.append(v);
+              columnCounter++;
+            } else {
+              if (columnCounter == 3) {
+                column3.append(v);
+                columnCounter = 1;
+              }
             }
           }
-        }
-      });
+        });
+      } else {
+        // Remove all items from grid
+        unmatched.detach();
+        matched.detach();
+        // Set button active
+        $(this).addClass("active");
+        // Sort matched items back into columns
+        var columnCounter = 1;
+        //
+        $.each(matched, function(i, v) {
+          if (columnCounter == 1) {
+            column1.append(v);
+            columnCounter++;
+          } else {
+            if (columnCounter == 2) {
+              column2.append(v);
+              columnCounter++;
+            } else {
+              if (columnCounter == 3) {
+                column3.append(v);
+                columnCounter = 1;
+              }
+            }
+          }
+        });
+      }
     }
   });
 });
