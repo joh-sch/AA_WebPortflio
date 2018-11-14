bf17ae438b13da7501a297b077df235211c72c2a
JoSch <j.schmoll@udk-berlin.de>
2018-11-14 22:18:13 +0100

    added basic markup of new grid-item overlay layout

--- AA-Web-Dev_01-1/site/snippets/snippets_content/aa-snippet-content-grid.php
+++ AA-Web-Dev_01-1/site/snippets/snippets_content/aa-snippet-content-grid.php
@@ -27,47 +27,36 @@ $projects_col3 = page('aa-projekte')->children()->visible()->filterBy('column', 
           <?php } ?>	
         </div>
         <!-- Item Overlay -->
-        <div class="grid-item--overlay <?= $project->similar() ->isNotEmpty()? 'hasButton':'' ?>">
-          <!-- Project Link -->
-            <a href="<?= $project->url() ?>">
-              <!-- »Show similar« Btn -->
-              <?php if($project->similar()->isNotEmpty()): ?>
-                <div class="button showSimilar z--high filters--tag" onClick="">
-                  <button data-filter=".<?= $project->similar() ?>">
-                    <span>show similar</span>
-                  </button>
-                </div>
-              <?php endif ?>
-              <!-- –––––––––––––––––– -->
-            </a>
-          <!-- –––––––––––– -->
-
-          <!-- Project Year -->
-          <div class="grid-item--overlay--text-container">
-            <div class="grid-item--overlay--text text--md year">
-              <?= $project->year()->kirbytext() ?>
+        <div class="grid-item--overlay">
+          <!-- Headline/Year Container -->
+          <div class="text-wrapper overlay">
+            <div class="text-container overlay">
+              <div class="text--lg"><?= str::unhtml( $project->title()->kirbytext() ) ?></div>
+              <div class="text--sm"><?= $project->year()->kirbytext() ?></div>
             </div>
-          </div> 
-
-          <!-- – Project Description –– -->
-          <div class="grid-item--overlay--text-container">
-
-            <div class="grid-item--overlay--text title">
-              <!-- Title -->
-              <span class="text--md"><?= str::unhtml( $project->title()->kirbytext() ) ?> </span>
-              <!-- Client/Free Work -->
+            <div class="text-container overlay">
               <?php if($project->freeOrClient()->bool()): ?>
-                <p class="text--md">Personal Work</p>
+                <p class="text--sm">Personal Work</p>
                 <?php else: ?>
-                <p class="text--md"><?= str::unhtml( $project->client()->kirbytext() ) ?></p>
+                <p class="text--sm"><?= str::unhtml( $project->client()->kirbytext() ) ?></p>
               <?php endif ?>
-            </div>
-            
-            <div class="grid-item--overlay--text text--md description">
-              <span><?= $project->description()->kirbytext() ?></span>
             </div>
           </div>
-          <!-- –––––––––––––––––– -->
+          <!-- Description/Button Container -->
+          <div class="button-container overlay">
+            <!-- Description -->
+            <div class="text--sm"><?= $project->description()->kirbytext() ?></div>
+            <!-- Divider -->
+            <div class="text-divider overlay"></div>
+            <!-- Button -->
+            <?php if($project->similar()->isNotEmpty()): ?>
+              <div class="button showSimilar z--high">
+                <button data-filter=".<?= $project->similar() ?>">
+                  <span>show similar</span>
+                </button>
+              </div>
+            <?php endif ?>
+          </div>
         </div>
         <!-- Image -->
         <img src="<?= $project->images()->filterBy('filename', '*=', 'cover')->first()->url() ?>" alt="Thumbnail for <?= $project->title()->html() ?>" class="showcase-image" />
