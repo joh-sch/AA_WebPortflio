241e908b697a90189738ae4c2c47cd35a48da815
JoSch <j.schmoll@udk-berlin.de>
2018-11-15 15:28:43 +0100

    type size adjustment

--- AA-Web-Dev_01-1/site/snippets/snippets_menus/aa-snippet-navbar.php
+++ AA-Web-Dev_01-1/site/snippets/snippets_menus/aa-snippet-navbar.php
@@ -1,7 +1,7 @@
 <div class="menu--navbar--top-wrapper z--topmost menu--state--closed" id="MainMenu">
 
 	<div class="menu--navbar--section_md hidden--vis <?php e($page->isActive(), 'active') ?>" id="MainMenu_work">
-		<div class="text--lg">
+		<div class="text--max">
 			<a href="/aa-home/">
 				<span>work</span>
 			</a>
@@ -9,7 +9,7 @@
 	</div>
 
 	<div class="menu--navbar--section_md hidden--vis" id="MainMenu_about">
-		<div class="text--lg">
+		<div class="text--max">
 			<a href="/about/">
 				<span>about</span>
 			</a>
@@ -17,13 +17,13 @@
 	</div>
 
 	<div class="menu--navbar--section_md hidden--vis" id="MainMenu_contact">
-		<div class="text--lg">
+		<div class="text--max">
 			<span>contact</span>
 		</div>
 	</div>
 
 	<div class="menu--navbar--section_md hidden--vis" id="MainMenu_shop">
-		<div class="text--lg">
+		<div class="text--max">
 			<span>shop</span>
 		</div>
 	</div>
